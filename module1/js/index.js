6dc214bc92177f379efe3b0f6610b0fe167cab61
Yuliya <y.malayok@gmail.com>
2018-06-15 13:36:43 +0200

    2nd

--- module1/js/index.js
+++ module1/js/index.js
@@ -1,17 +1,20 @@
-const ADMIN_LOGIN = prompt('Ваш логин');
+const ADMIN_LOGIN = 'admin';
+const ADMIN_PASSWORD = 'm4ngo1zh4ackz0r';
+let password;
 const cancelled = 'Отменено пользователем!';
 const denied = 'Доступ запрещен!';
+const login = prompt('Ваш логин');
 
-if (ADMIN_LOGIN === 'admin') {
-  const ADMIN_PASSWORD = prompt('Ваш пароль');
-  if (ADMIN_PASSWORD === null) {
+if (login === ADMIN_LOGIN) {
+  password = prompt('Ваш пароль');
+  if (password === null) {
     alert(cancelled);
-  } else if (ADMIN_PASSWORD === 'm4ngo1zh4ackz0r') {
+  } else if (password === ADMIN_PASSWORD) {
     alert('Добро пожаловать!');
   } else {
     alert(denied)
   }
-} else if (ADMIN_LOGIN === null) {
+} else if (login === null) {
   alert(cancelled);
 } else {
   alert(denied);
